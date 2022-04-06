16f669e7a08b8d7c7f512f740cdca7b6a16dffc6
Giovanni Chen <gchen.falcone@gmail.com>
2022-04-06 09:46:02 +0200

    refactor: moved files in correct folders

--- Intermadiate/Day22/main.py
+++ Intermadiate/Day22/main.py
@@ -1,34 +1,42 @@
-import time
 from turtle import Screen
-from player import Player
-from car_manager import CarManager
+from paddle import Paddle
+from ball import Ball
 from scoreboard import Scoreboard
-
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
-screen.listen()
+import time
 
 game_is_on = True
-player = Player()
-car_manager = CarManager()
+screen = Screen()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
 scoreboard = Scoreboard()
 
-screen.onkeypress(key="Up", fun=player.move)
+screen.setup(height=600, width=800)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.listen()
+screen.tracer(0)
+screen.onkeypress(key="Up", fun=r_paddle.up)
+screen.onkeypress(key="Down", fun=r_paddle.down)
+screen.onkeypress(key="w", fun=l_paddle.up)
+screen.onkeypress(key="s", fun=l_paddle.down)
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.01)
     screen.update()
-    car_manager.generate_car()
-    car_manager.move()
+    ball.move_ball()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 
-    for car in car_manager.cars:
-        if player.distance(car) < 20:
-            game_is_on = False
-            scoreboard.game_over()
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 330) or (ball.distance(l_paddle) < 50 and ball.xcor() < -330):
+        ball.bounce_puddle()
 
-    if player.end_road():
-        scoreboard.increase_level()
-        car_manager.increase_speed()
+    if ball.xcor() < -410:
+        scoreboard.r_point()
+        ball.reset()
+
+    if ball.xcor() > 410:
+        scoreboard.l_point()
+        ball.reset()
 
 screen.exitonclick()
