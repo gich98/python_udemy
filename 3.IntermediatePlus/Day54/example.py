cb4cb3af9a2ca132035ccdc9ccb9da8e8a63e004
Giovanni Chen <gchen.falcone@gmail.com>
2022-05-02 20:04:50 +0200

    feat: Intro Flask and Decorators

---
+++ 3.IntermediatePlus/Day54/example.py
@@ -0,0 +1,29 @@
+import time
+
+
+def delay_decorator(function):
+    def wrapper_function():
+        time.sleep(2)
+        # Do something before
+        function()
+        # Do something after
+    return wrapper_function
+
+
+@delay_decorator
+def say_hello():
+    print("Hello")
+
+
+@delay_decorator
+def say_bye():
+    print("Bye")
+
+
+def say_greetings():
+    print("Greetings")
+
+
+say_hello()
+say_greetings()
+say_bye()
